ccafdb12bb5e6164eeecf81caa2bbd5f292e2f5d
Bernardo Chamilet <bernardoboggionechamilet@gmail.com>
2025-04-14 03:13:32 -0300

    Implementação controller de gerar artigo

--- src/controllers/crewai_controllers.py
+++ src/controllers/crewai_controllers.py
@@ -1,6 +1,8 @@
 from flask import request, jsonify
 import src.models.subject_model as models
+from src.models.article_model import Article
 from pydantic import ValidationError
+from src.crew.crew import artigo_crew
 
 # generateArticle recebe um assunto e gera um artigo com no mínimo 300 palavras sobre ele
 def generateArticle():
@@ -8,8 +10,19 @@ def generateArticle():
     req_body = request.get_json()
     # Validando assunto do artigo
     try:
-         subject = models.Subject(**req_body)
+        subject = models.Subject(**req_body)
     except ValidationError as e:
         return jsonify({"erro": e.errors()}), 400
-    subject = subject.model_dump()
-    return jsonify(subject), 200
+    # Executando a crew 
+    try:
+        artigo = artigo_crew.kickoff(inputs={"subject": subject.subject})
+    except Exception as e:
+        return jsonify({"erro": f"Erro ao gerar o artigo: {str(e)}"}), 500
+    # Validando output da crew
+    try:
+        resultado = Article(**artigo.result)
+        resultado_dict = resultado.model_dump()
+    except ValidationError as e:
+        return jsonify({"erro": e.errors()}), 400
+    # 200: sucesso
+    return jsonify(resultado_dict), 200
--- src/crew/llm/gemini.py
+++
@@ -1,9 +0,0 @@
-import os
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-# Configurando a LLM Gemini
-llm_gemini = ChatGoogleGenerativeAI(
-    model="gemini-pro",
-    google_api_key=os.getenv("GOOGLE_API_KEY"),
-    temperature=0.7
-)
