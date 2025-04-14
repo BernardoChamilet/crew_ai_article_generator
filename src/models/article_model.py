0d1916e42ead9fb003d798350a1f94a62fc9b980
Bernardo Chamilet <bernardoboggionechamilet@gmail.com>
2025-04-13 22:04:31 -0300

    Ajuste modelo de artigo esperado

--- src/models/article_model.py
+++ src/models/article_model.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel, Field, field_validator
+from typing import Optional
 
 # Article é uma classe que representa o artigo gerado pela crew e é usada para validação de dados
 class Article(BaseModel):
     title: str = Field(..., min_length=5)
     content: str = Field(..., min_length=600)
-    sources: list[str] = Field(min_length=1)
+    sources: Optional[list[str]] = None
     @field_validator('content')
     @classmethod
     def validate_number_of_words(cls, content):
