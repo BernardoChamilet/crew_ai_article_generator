89dda3485865169ae8f560132aee65a3e4248c16
Bernardo Chamilet <bernardoboggionechamilet@gmail.com>
2025-04-15 02:45:33 -0300

    Mudança da versão do llama

--- src/crew/llm/groq.py
+++ src/crew/llm/groq.py
@@ -3,7 +3,7 @@ from src.config.config import groq_api_key
 
 # Configurando a LLM
 llm_groq = LLM(
-    model="groq/llama3-8b-8192",
+    model="groq/llama3-70b-8192",
     api_key=groq_api_key,
     temperature=0.7
 )
